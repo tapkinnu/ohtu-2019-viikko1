9be275a5afacb030077c3d7e72f7d8f4ae5cd745
Tapio Kinnunen <ganomix@gmail.com>
2019-11-09 18:28:17 +0200

    viikko 2 tehtävä 3

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+package ohtu.ohtuvarasto;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        if (1 < 2)
+		{
+			System.out.print("1 on pienempi kuin 2");
+		}
+    }
+}
